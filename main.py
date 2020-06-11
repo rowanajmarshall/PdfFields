ef9a47507dcfdb4d9ee3eca1cf911a6f36016bd4
Rowan Marshall <rowanajmarshall@gmail.com>
2020-06-11 23:38:46 +0100

    Script to show form fields on a PDF

---
+++ main.py
@@ -0,0 +1,45 @@
+import sys
+from typing import List
+
+from PyPDF2.generic import Field
+
+try:
+    from PyPDF2 import PdfFileReader
+except ImportError:
+    print("Please install PyPDF2 e.g. `pip3 install PyPDF2`")
+    exit(1)
+
+
+def readable_type(field: Field) -> str:
+    if field.fieldType == "/Tx":
+        return "Text"
+    elif field.fieldType == "/Ch":
+        return "Combo box"
+    elif field.fieldType == "/Btn":
+        return "Check or Radio box"
+    elif field.fieldType == "/Sig":
+        return "Signature bx"
+    else:
+        return "Unknown"
+
+
+def get_value(field: Field) -> str:
+    if field.value is None:
+        return ""
+    else:
+        return field.value
+
+
+def main(args: List[str]) -> int:
+    filename = args[1]
+    reader = PdfFileReader(filename)
+    fields = reader.getFields()
+    for f in fields.keys():
+        print(f"Name: '{f}', "
+              f"Type: '{readable_type(fields[f])}', "
+              f"Value: {get_value(fields[f])}")
+    return 0
+
+
+if __name__ == "__main__":
+    exit(main(sys.argv))
